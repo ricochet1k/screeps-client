5e7e699b344921a79c92507ff0dd60e5204529cd
Matt Peterson <mattpeterson@google.com>
2017-04-04 17:08:01 -0400

    Scale energy better

--- src/scripts/objects/energy.js
+++ src/scripts/objects/energy.js
@@ -13,8 +13,12 @@ export default class Energy extends GameObject {
 
 		let g = this.g;
 
+		const e = obj.energy;
+		// this produces a number between 0 and 1, approaching 1 as e approaches infinity
+		const scale = e / (e + 100);
+
 		g.beginFill(0xffff00);
-		g.drawCircle(S(5), S(5), S(4) * obj.energy / 1000);
+		g.drawCircle(S(5), S(5), Math.min(S(5), Math.max(1, S(4) * scale)));
 		g.endFill();
 	}
 }
